4ceb98292f9eeecf70e03fd56ec03b317b1f30a8
Kieran Chambers <kieranc0808@gmail.com>
2025-01-28 20:04:20 +0000

    completed caching section of course

---
+++ app/messages/new/page.js
@@ -0,0 +1,30 @@
+import { addMessage } from "@/lib/messages";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default function NewMessagesPage() {
+    async function createMessage(formData) {
+        'use server';
+
+        const message = formData.get('message');
+        addMessage(message);
+        revalidateTag('msg');
+        redirect('/messages');
+    }
+
+    return (
+        <>
+            <h2>New Message</h2>
+            <form action={createMessage}>
+                <p className="form-control">
+                    <label htmlFor="message">Your Message</label>
+                    <textarea id="message" name="message" required rows="5" />
+                </p>
+
+                <p className="form-actions">
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </>
+    );
+}
